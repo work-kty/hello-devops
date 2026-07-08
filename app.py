deb64a998eae010f0e93c09bd016f06e4cffa52f
work-kty <workkty92@gmail.com>
2026-07-08 16:31:28 +0900

    문구 수정

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  return "Hello DevOps! version 1"
+  return "Hello DevOps! version 1.1"
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=5000)
